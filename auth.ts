e10cfbde9df477e90dc8984c91a701f24fdfcbf9
hatrongthuy <booha061294@gmail.com>
2026-08-13 22:20:21 +0700

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -16,8 +16,11 @@ declare module "next-auth" {
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     Google({
+      clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
       authorization: {
         params: {
           // Giới hạn theo domain công ty ngay tại màn hình chọn tài khoản Google.
