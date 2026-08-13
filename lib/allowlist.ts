44156e22c345c604c48a88ec17bda1e2ba6da896
hatrongthuy <booha061294@gmail.com>
2026-08-13 23:07:41 +0700

    Update allowlist.ts

--- lib/allowlist.ts
+++ lib/allowlist.ts
@@ -21,7 +21,7 @@ export const COMPANY_DOMAIN = "cpc1hn.com.vn";
 
 export const ALLOWLIST: AllowlistEntry[] = [
   {
-    email: "thuy.hatrong.ss.ps@cpc1hn.com.vn",
+    email: "booha061294@gmail.com",",
     hoTen: "Hà Trọng Thủy",
     role: "manager",
   },
